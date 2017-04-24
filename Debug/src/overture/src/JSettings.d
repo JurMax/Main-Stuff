53763f2e221eb29c7c89f3322fb1ce4b4169ff6f
maximvdberg <maximvdberg@gmail.com>
2017-04-24 22:03:28 +0200

    Removed debug folder

--- Debug/src/overture/src/JSettings.d
+++
@@ -1,4 +0,0 @@
-src/overture/src/JSettings.o: ../src/overture/src/JSettings.cpp \
-  /Users/jurriaanvandenberg/Documents/Programming/C++/WeeklyGames/Main\ Project/src/overture/include/JSettings.hpp
-
-/Users/jurriaanvandenberg/Documents/Programming/C++/WeeklyGames/Main\ Project/src/overture/include/JSettings.hpp:
--- Debug/src/redox/objects/particles/elektron.d
+++
@@ -1,5 +0,0 @@
-src/redox/objects/particles/elektron.o: \
-  ../src/redox/objects/particles/elektron.cpp \
-  ../src/redox/objects/particles/elektron.hpp
-
-../src/redox/objects/particles/elektron.hpp:
